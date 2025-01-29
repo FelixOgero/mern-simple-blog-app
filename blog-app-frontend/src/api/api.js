3d3f0f28dd63f89915b23ba69266e252f9c2ac6c
FelixOgero <ogerofelix2001@gmail.com>
2025-01-29 11:06:28 +0300

    added vercel api url

--- blog-app-frontend/src/api/api.js
+++ blog-app-frontend/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
+  // baseURL: 'http://localhost:5000/api', // Backend URL
+  baseURL: 'https://mern-simple-blog-app.vercel.app/api', // Backend URL
 });
 
 // Attach token to requests if available
